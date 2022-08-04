d9d7bb141f3a5961d72f257a3aa40f2d36671de2
AbdullahShaukat474 <96533390+AbdullahShaukat474@users.noreply.github.com>
2022-08-04 11:31:35 +0500

    Update function.php

--- function.php
+++ function.php
@@ -28,5 +28,6 @@ writeMsg();
 
 </body>
 <h1>  This is dev2 Code  </h1>
+  <h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1><h1>  This is dev2 Code  </h1>
 </html>
 
